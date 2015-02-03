cc89418744ab3f2a533de9e88b4cefb30552f749
Romain Dauby <romain.dauby@gmail.com>
2015-02-03 10:15:29 +0100

    Fullscreen webview

--- app/src/main/java/fr/followforfollow/followforfollow/MainActivity.java
+++ app/src/main/java/fr/followforfollow/followforfollow/MainActivity.java
@@ -1,20 +1,25 @@
 package fr.followforfollow.followforfollow;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.WebView;
 import android.webkit.WebSettings;
 import android.webkit.WebViewClient;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
     private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         mWebView = (WebView) findViewById(R.id.activity_main_webview);
